380e01933e5ac678b019bd77fc9e8da5723ad5fd
phidangnguyen <phidang0210@gmail.com>
2024-12-08 15:01:04 +0700

    vesting interface

--- contracts/vesting/Vesting.sol
+++ contracts/vesting/Vesting.sol
@@ -7,33 +7,12 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import "./interfaces/IFactory.sol";
+import "./interfaces/IVesting.sol";
 import "hardhat/console.sol";
 
-contract Vesting is ReentrancyGuard, Pausable {
+contract Vesting is IVesting, ReentrancyGuard, Pausable {
     using SafeMath for uint256;
     using EnumerableSet for EnumerableSet.AddressSet;
-
-    struct VestingSchedule {
-        uint256 startTime; // First/TGE vesting time
-        uint256 firstUnlockPercentage; // TGE % unlock 1000 ~ 1%
-        uint256 lockDuration; // ex:  30 days, locked after TGE time
-        uint256 vestingDuration; // ex: 18 months linear vesting
-        uint256 vestingPeriods; // Number of vesting periods (e.g., 18)
-        uint256 period; // Period of time
-        uint256 periodUnlockPercentage; // period % unlock
-        uint256 endTime; // End of vesting
-        uint256 totalPoolCap;
-        uint256 totalReleasedAmount;
-    }
-
-    struct UserInfo {
-        uint256 totalAmount;
-        bool isFirstReleaseClaimed;
-        uint256 firstReleaseAmount;
-        uint256 amountPerPeriod;
-        uint256 releasedAmount;
-        uint256 completedPeriods;
-    }
 
     uint256 HUNDRED_PERCENT = 100000; // ~ 100%
 
@@ -42,14 +21,6 @@ contract Vesting is ReentrancyGuard, Pausable {
     VestingSchedule private __vestingSchedule; // Vesting infomation
     mapping(address => UserInfo) private __users; // Users infos storage
     EnumerableSet.AddressSet __poolUsers;
-
-    event Initialized(VestingSchedule info);
-    event BeneficiaryAdded(address beneficiary, UserInfo user);
-    event Released(
-        address beneficiary,
-        uint256 releasedAmount,
-        uint256 releasedPeriods
-    );
 
     constructor() {
         factory = msg.sender;
